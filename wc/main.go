d47b0f812f873dc2b33b22c0e3148be56e23c2f0
Felipe <26683783+feslima@users.noreply.github.com>
2023-10-22 17:44:22 -0300

    fix: main function call

--- wc/main.go
+++ wc/main.go
@@ -53,5 +53,27 @@ func getResultsReport(configs WcConfigs, results WcResult) string {
 }
 
 func main() {
-	println("Hello!")
+	configs := WcConfigs{in: nil, shouldCountBytes: false}
+	configs.getFlags()
+
+	if filename := flag.Arg(0); filename != "" {
+		file, err := openFile(filename)
+		if err != nil {
+			fmt.Println("Expected to open file without errors. err:", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+
+		configs.in = file
+	} else {
+		configs.in = os.Stdin
+	}
+
+	results, err := DoWc(configs.in)
+	if err != nil {
+		fmt.Println("Failed to perform word count. err:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(getResultsReport(configs, results))
 }
