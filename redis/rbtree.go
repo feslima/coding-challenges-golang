9fb8a9616903eb3a266dc62c4a37508d79626d85
Felipe <26683783+feslima@users.noreply.github.com>
2023-11-25 11:31:08 -0300

    refactor(redis): follow Sedgewick's implementation

--- redis/rbtree.go
+++ redis/rbtree.go
@@ -10,21 +10,20 @@ const (
 )
 
 type node[k cmp.Ordered, v any] struct {
-	key   k
-	value v
-	left  *node[k, v]
-	right *node[k, v]
-	color color
+	key      k
+	value    v
+	left     *node[k, v]
+	right    *node[k, v]
+	color    color
+	numNodes int
 }
 
 type tree[k cmp.Ordered, v any] struct {
 	root *node[k, v]
-	size int
 }
 
 func NewTree[k cmp.Ordered, v any]() *tree[k, v] {
 	return &tree[k, v]{
-		size: 0,
 		root: nil,
 	}
 }
@@ -84,9 +83,12 @@ func (t *tree[k, v]) Put(key k, val v) {
 
 func (t *tree[k, v]) put(key k, val v, n *node[k, v]) *node[k, v] {
 	if n == nil {
-		newNode := &node[k, v]{key: key, value: val, color: RED}
-		t.size++
-		return newNode
+		return &node[k, v]{
+			key:      key,
+			value:    val,
+			color:    RED,
+			numNodes: 1,
+		}
 	}
 
 	if n.key > key {
@@ -109,97 +111,48 @@ func (t *tree[k, v]) put(key k, val v, n *node[k, v]) *node[k, v] {
 		flipColors(n)
 	}
 
+	n.numNodes = size(n.left) + size(n.right) + 1
 	return n
 }
 
-func (t *tree[k, v]) Remove(key k) v {
-	removed := t.remove(key, t.root, t.root)
-
-	var result v
-	if removed == nil {
-		return result
-	}
-
-	return removed.value
+func (t *tree[k, v]) Remove(key k) {
+	t.root = t.remove(key, t.root)
 }
 
-func (t *tree[k, v]) remove(key k, n *node[k, v], parent *node[k, v]) *node[k, v] {
+func (t *tree[k, v]) deleteMin(n *node[k, v]) *node[k, v] {
+	if n.left == nil {
+		return n.right
+	}
+
+	n.left = t.deleteMin(n.left)
+	n.numNodes = size(n.left) + size(n.right) + 1
+	return n
+}
+
+func (t *tree[k, v]) remove(key k, n *node[k, v]) *node[k, v] {
 	if n == nil {
 		return nil
 	}
 
 	if n.key > key {
-		return t.remove(key, n.left, n)
-	}
-	if n.key < key {
-		return t.remove(key, n.right, n)
-	}
-
-	markedForDeletion := &node[k, v]{key: n.key, value: n.value}
-	t.size -= 1
-
-	// if the node being deleted has no children, simply delete it
-	if n.right == nil && n.left == nil {
-		if parent.left.key == n.key {
-			parent.left = nil
+		n.left = t.remove(key, n.left)
+	} else if n.key < key {
+		n.right = t.remove(key, n.right)
+	} else {
+		if n.right == nil {
+			return n.left
 		}
-		if parent.right.key == n.key {
-			parent.right = nil
+		if n.left == nil {
+			return n.right
 		}
-		return markedForDeletion
-	}
 
-	/* if the node being deleted has one child, delete the node and
-	plug the child in the spot where the deleted node was */
-	if n.right != nil && n.left == nil {
-		n.key = n.right.key
-		n.value = n.right.value
-		n.right = nil
-		return markedForDeletion
+		z := n
+		n = t.min(z.right)
+		n.right = t.deleteMin(z.right)
+		n.left = z.left
 	}
-
-	if n.left != nil && n.right == nil {
-		n.key = n.left.key
-		n.value = n.left.value
-		n.left = nil
-		return markedForDeletion
-	}
-
-	// if the node has two children, replace the node with the successor node
-	successorAndParent := t.findSuccessorWithParent(n.right, n)
-	successor := successorAndParent[0]
-	successorParent := successorAndParent[1]
-	n.key = successor.key
-	n.value = successor.value
-	if successorParent.left != nil && successorParent.left.key == successor.key {
-		successorParent.left = nil
-	}
-	if successorParent.right != nil && successorParent.right.key == successor.key {
-		successorParent.right = nil
-	}
-
-	/* if the successor node has a right child, after plugging the
-	successor node into the spot of the deleted node, take the former
-	child of the successor node and turn into the left child of
-	the former parent of the  successor node
-	*/
-	if successor.right != nil {
-		successorParent.left = successor.right
-	}
-
-	return markedForDeletion
-}
-
-// Keep visiting the left child until there is no more child left, then return the childless node
-func (t tree[k, v]) findSuccessorWithParent(n *node[k, v], parent *node[k, v]) []*node[k, v] {
-	result := []*node[k, v]{}
-	if n.left == nil {
-		result = append(result, n)
-		result = append(result, parent)
-		return result
-	}
-
-	return t.findSuccessorWithParent(n.left, n)
+	n.numNodes = size(n.left) + size(n.right) + 1
+	return n
 }
 
 func (t tree[k, v]) GetKeySet() []k {
@@ -208,8 +161,15 @@ func (t tree[k, v]) GetKeySet() []k {
 	return keys
 }
 
-func (t tree[key, v]) Size() int {
-	return t.size
+func (t tree[k, v]) Size() int {
+	return size(t.root)
+}
+
+func size[k cmp.Ordered, v any](n *node[k, v]) int {
+	if n == nil {
+		return 0
+	}
+	return n.numNodes
 }
 
 func (t tree[k, v]) inOrderTraversal(n *node[k, v], collector *[]k) {
@@ -235,6 +195,9 @@ func rotateLeft[k cmp.Ordered, v any](h *node[k, v]) *node[k, v] {
 	x.left = h
 	x.color = h.color
 	h.color = RED
+
+	x.numNodes = h.numNodes
+	h.numNodes = size(h.left) + size(h.right) + 1
 	return x
 }
 
@@ -244,6 +207,9 @@ func rotateRight[k cmp.Ordered, v any](h *node[k, v]) *node[k, v] {
 	x.right = h
 	x.color = h.color
 	h.color = RED
+
+	x.numNodes = h.numNodes
+	h.numNodes = size(h.left) + size(h.right) + 1
 	return x
 }
 
